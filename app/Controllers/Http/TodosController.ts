95cb667ba6c6a597bfbe6c7a6d35c6d72ec22c74
fandi-ahmad <fandi4160@gmail.com>
2023-04-14 21:21:36 +0800

    filter get data

--- app/Controllers/Http/TodosController.ts
+++ app/Controllers/Http/TodosController.ts
@@ -4,11 +4,24 @@ import Todo from "App/Models/Todo";
 
 export default class TodosController {
     public async index({request}) {
-        // return Todo.all()        ambil semua data tanpa filter
-        const page = request.input('page', 1)
-        const limit = request.input('limit', 3)
+        // ambil semua data tanpa filter
+        // return Todo.all()
 
+        // get data dengan filter
+        const page = request.input('page', 1)   // angka di parameter kedua adalah value default jika parameter kosong
+        const limit = request.input('limit', 0)
         return Todo.query().paginate(page, limit)
+
+        // sebagai fungsi search, get data berdasarkan id
+        // const find = request.input('find', 1)
+        // const todos = await Todo.find(find)
+        // return todos?.serialize()
+
+        // get semua data, tapi hanya menampilkan yang di dalam fields:[]
+        // const todos = await Todo.all()
+        // return todos.map(todo => todo.serialize({
+        //     fields:['id', 'title']
+        // }))
     }
 
     public async store({request, response}:HttpContextContract) {
